513c15af4aa1fdae2833435bb415480bc1b9f54b
timuraminov77 <timur.aminov.2020@mail.ru>
2026-04-05 22:11:48 +0400

    remove ADMIN_ID from file

--- config.py
+++ config.py
@@ -18,4 +18,4 @@ DB_NAME     = os.getenv("DB_NAME", "restaurant")
 BASE_DIR = Path(__file__).parent  # всегда корень проекта, независимо от запуска
 CHROMA_PATH = os.getenv("CHROMA_PATH", str(BASE_DIR / "RAG" / "chroma_db"))
 INFO_MD_PATH = os.getenv("INFO_MD_PATH", "./info.md")
-ADMIN_TG_ID = 7533405423
+ADMIN_TG_ID = int(os.getenv('ADMIN_TG_ID', 0))
